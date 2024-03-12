e618242f273bce9ee8d82f415ef5980eaa6868ea
Slava Sidorov <sidorov-vv3@mail.ru>
2024-03-13 02:05:11 +0700

    added section popular and added swiper

--- src/js/index.js
+++ src/js/index.js
@@ -1,8 +1,19 @@
-import autoCompleteFunc from "./modules/autoComplete";
-import picker from "./modules/calendarPicker";
+// import autoCompleteFunc from "./modules/autoComplete";
+// import picker from "./modules/calendarPicker";
 
-autoCompleteFunc();
-picker();
+// autoCompleteFunc();
+// picker();
 // import mobileNav from './modules/mobile-nav.js';
 // mobileNav();
 
+import Swiper from 'swiper/bundle';
+import 'swiper/css/bundle';
+
+const swiper = new Swiper('.swiper', {
+  slidesPerView: 4,
+  spaceBetween: 32,
+  navigation: {
+    nextEl: '#sliderNext',
+    prevEl: '#sliderPrev',
+  },
+});  
